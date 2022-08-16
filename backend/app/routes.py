78cfb9c27e9d8ebdab78e731cd2b72109ae78e80
= <seth.babauta5@gmail.com>
2022-08-16 14:41:02 -0700

    Connected backend to frontend

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,5 +1,9 @@
+from flask import url_for
 from app import app
+import json
 
-@app.route("/get_query")
-def get_query():
-    return "Hello world"
+@app.route("/api/homepage")
+def index():
+    with open("app/json/homepage_data.json") as f:
+        data = json.load(f)
+        return data
